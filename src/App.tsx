ba454bedab6a9bfaded63301d1bfde63061ccd9e
thecodedguyy <prajapratik1998@gmail.com>
2024-01-28 03:47:44 +0530

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 function App() {
   const [length, setLength] = useState<number>(8);
